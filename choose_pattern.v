2cd2ec04bc95c14f038d944ee6950c0e1bdf48d0
SimonLiu <simonliu423@gmail.com>
2023-12-27 23:05:43 +0800

    Feat: choose pattern with keypad

---
+++ choose_pattern.v
@@ -0,0 +1,40 @@
+module choose_pattern(clk, rst, keypadRow, keypadCol, pattern);
+	input				clk, rst;
+	input		[3:0]	keypadCol;
+	output reg	[3:0]	keypadRow, pattern;
+
+	always @(posedge clk or negedge rst) begin
+		if (!rst) begin
+			keypadRow <= 4'b1110;
+			pattern <= 4'h0;
+		end
+		else begin
+			case ({keypadRow, keypadCol})
+				8'b0111_0111: pattern <= 4'hf;
+				8'b0111_1011: pattern <= 4'he;
+				8'b0111_1101: pattern <= 4'hd;
+				8'b0111_1110: pattern <= 4'hc;
+				8'b1011_0111: pattern <= 4'hb;
+				8'b1011_1011: pattern <= 4'h3;
+				8'b1011_1101: pattern <= 4'h6;
+				8'b1011_1110: pattern <= 4'h9;
+				8'b1101_0111: pattern <= 4'ha;
+				8'b1101_1011: pattern <= 4'h2;
+				8'b1101_1101: pattern <= 4'h5;
+				8'b1101_1110: pattern <= 4'h8;
+				8'b1110_0111: pattern <= 4'h0;
+				8'b1110_1011: pattern <= 4'h1;
+				8'b1110_1101: pattern <= 4'h4;
+				8'b1110_1110: pattern <= 4'h7;
+				default: pattern <= pattern;
+			endcase
+			case (keypadRow)
+				4'b0111: keypadRow <= 4'b1011;
+				4'b1011: keypadRow <= 4'b1101;
+				4'b1101: keypadRow <= 4'b1110;
+				4'b1110: keypadRow <= 4'b0111;
+				default: keypadRow <= 4'b1110;
+			endcase
+		end
+	end
+endmodule
--- main.v
+++ main.v
@@ -1,25 +1,43 @@
 `define TimeExpire_VGA 32'd0
 `define TimeExpire_Game 32'd12500000
 `define TimeExpire_Movement 32'd10000
+`define TimeExpire_Keypad 32'd250000
+`define TimeExpire_Matrix 32'd2500
 `define MAX_X 8'd64
 `define MAX_Y 8'd48
 
-module main(clk, rst, freeze, move, r_out, g_out, b_out, hsync, vsync);
+module main(clk, rst, freeze, move, keypadRow, keypadCol, r_out, g_out, b_out, hsync, vsync);
 	input				clk, rst;
 	input				freeze;
 	input	[3:0]		move;
+	input	[3:0]		keypadCol;
+
 	output				hsync, vsync;
 	output	[3:0]		r_out, g_out, b_out;
+	output	[3:0]		keypadRow;
 
-	wire				vga_clk, game_clk, move_clk;
+	wire				vga_clk, game_clk, move_clk, key_clk, mat_clk;
 	wire	[3071:0]	state;
+	wire	[3:0]		pattern_idx;
+	wire	[63:0]		pattern_mat;
 	wire	[7:0]		cursor_x, cursor_y;
 
-	clk_div			M0(clk, rst, `TimeExpire_VGA, vga_clk);
-	clk_div			M00(clk, rst, `TimeExpire_Game, game_clk);
-	clk_div			M000(clk, rst, `TimeExpire_Movement, move_clk);
-	move_cursor		M12(move_clk, rst, move, cursor_x, cursor_y);
-	conway_fsm		M1(game_clk, rst, freeze, state);
+	// clks
+	clk_div			A0(clk, rst, `TimeExpire_VGA, vga_clk);
+	clk_div			B0(clk, rst, `TimeExpire_Game, game_clk);
+	clk_div			C0(clk, rst, `TimeExpire_Movement, move_clk);
+	clk_div			D0(clk, rst, `TimeExpire_Keypad, key_clk);
+	clk_div			E0(clk, rst, `TimeExpire_Matrix, mat_clk);
+
+	// inputs
+	move_cursor		M1(move_clk, rst, move, cursor_x, cursor_y);
+	choose_pattern	N1(key_clk, rst, keypadRow, keypadCol, pattern_idx);
+
+	// states
+	conway_fsm		F1(game_clk, rst, freeze, state);
+	load_pattern	G1(clk, rst, pattern_idx, pattern_mat);
+
+	// outputs
 	display			M2(vga_clk, rst, state, r_out, g_out, b_out, hsync, vsync);
 endmodule
 
