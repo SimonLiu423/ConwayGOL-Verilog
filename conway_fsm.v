00da88206f7101248a545d6655713d538f506ab1
SimonLiu <simonliu423@gmail.com>
2023-12-28 00:28:04 +0800

    Refactor: remove array

--- conway_fsm.v
+++ conway_fsm.v
@@ -2,10 +2,10 @@ module conway_fsm(clk, rst, freeze, state);
 	input					clk, rst;
 	input					freeze;
 	output reg	[3071:0]	state;
-	reg			[63:0]		state_cp [47:0];
+	reg			[3071:0]	state_cp;
 
-	integer i, j, di, dj, ni, nj;
-	integer live_neighbors;
+	integer x, y, dx, dy, nx, ny;
+	integer idx, live_neighbors;
 
 	always @(posedge clk or negedge rst) begin
 		if (!rst) begin
@@ -16,35 +16,31 @@ module conway_fsm(clk, rst, freeze, state);
 		end
 		else begin
 			if (freeze == 0) begin
-				for ( i = 0; i < 48; i = i + 1) begin
-					for ( j = 0; j < 64; j = j + 1) begin
-						state_cp[i][j] = state[i*48+j];
-					end
-				end
-
-				for ( i = 0; i < 48; i = i + 1) begin
-					for ( j = 0; j < 64; j = j + 1) begin
+				state_cp = state;
+				for ( y = 0; y < `MAX_Y; y = y + 1) begin
+					for ( x = 0; x < `MAX_X; x = x + 1) begin
+						idx = y*`MAX_Y + x;
 						live_neighbors = 0;
-						for ( di = -1; di <= 1; di = di + 1) begin
-							for ( dj = -1; dj <= 1; dj = dj + 1 ) begin
-								if (~(di == 0 && dj == 0)) begin
-									ni = i + di;
-									nj = j + dj;
-									if (ni >= 0 && ni < 48 && nj >= 0 && nj < 64) begin
-										live_neighbors = live_neighbors + state_cp[ni][nj];
+						for ( dy = -1; dy <= 1; dy = dy + 1) begin
+							for ( dx = -1; dx <= 1; dx = dx + 1 ) begin
+								if (~(dy == 0 && dx == 0)) begin
+									ny = y + dy;
+									nx = x + dx;
+									if (ny >= 0 && ny < `MAX_Y && nx >= 0 && nx < `MAX_X) begin
+										live_neighbors = live_neighbors + state_cp[ny*`MAX_Y + nx];
 									end	
 								end
 							end
 						end
 						
-						if (state_cp[i][j] == 1 && (live_neighbors < 2 || live_neighbors > 3)) begin
-        	        		state[i*48 + j] = 0; // Cell dies
+						if (state[idx] == 1 && (live_neighbors < 2 || live_neighbors > 3)) begin
+        	        		state[idx] = 0; // Cell dies
         	    		end
-						else if (state_cp[i][j] == 0 && live_neighbors == 3) begin
-							state[i*48 + j] = 1;
+						else if (state[idx] == 0 && live_neighbors == 3) begin
+							state[idx] = 1;
         	    		end
 						else begin
-							state[i*48 + j] = state_cp[i][j];
+							state[idx] = state[idx];
         	    		end
 					end
 				end
