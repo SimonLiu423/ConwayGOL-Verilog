af8b013be5fe0eddf472a9eaf9cf4f67bdd66fc9
SimonLiu <simonliu423@gmail.com>
2023-12-27 23:29:25 +0800

    Chore: update tcl script for pin

--- pin.tcl
+++ pin.tcl
@@ -27,3 +27,34 @@ set_location_assignment PIN_M6 -to move[3];
 
 # set freeze
 set_location_assignment PIN_U13 -to freeze;
+
+# set keypad row
+set_location_assignment PIN_J17 -to keypadRow[0];
+set_location_assignment PIN_G13 -to keypadRow[1];
+set_location_assignment PIN_G16 -to keypadRow[2];
+set_location_assignment PIN_F13 -to keypadRow[3];
+# set keypad col
+set_location_assignment PIN_F12 -to keypadCol[0];
+set_location_assignment PIN_G15 -to keypadCol[1];
+set_location_assignment PIN_G12 -to keypadCol[2];
+set_location_assignment PIN_K16 -to keypadCol[3];
+
+
+# set dot matrix
+set_location_assignment PIN_E15 -to dot_row[0];
+set_location_assignment PIN_B15 -to dot_row[1];
+set_location_assignment PIN_A12 -to dot_row[2];
+set_location_assignment PIN_E14 -to dot_row[3];
+set_location_assignment PIN_C13 -to dot_row[4];
+set_location_assignment PIN_B12 -to dot_row[5];
+set_location_assignment PIN_A13 -to dot_row[6];
+set_location_assignment PIN_D13 -to dot_row[7];
+
+set_location_assignment PIN_J11 -to dot_col[0];
+set_location_assignment PIN_J19 -to dot_col[1];
+set_location_assignment PIN_H18 -to dot_col[2];
+set_location_assignment PIN_A15 -to dot_col[3];
+set_location_assignment PIN_G18 -to dot_col[4];
+set_location_assignment PIN_H14 -to dot_col[5];
+set_location_assignment PIN_H10 -to dot_col[6];
+set_location_assignment PIN_G11 -to dot_col[7];
